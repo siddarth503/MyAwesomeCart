1b52d7f7d04e5524a9b08b8cc5f28d27bb771691
siddarth503 <130058882+siddarth503@users.noreply.github.com>
2023-04-06 17:53:23 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
